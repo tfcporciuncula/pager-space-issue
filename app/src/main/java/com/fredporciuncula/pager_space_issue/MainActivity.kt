32865f48271db115ac299141a6b0e358c047be9d
Fred Porciúncula <tfcporciuncula@gmail.com>
2023-10-12 15:33:08 +0200

    Add a pager with spaceBetweenPages

--- app/src/main/java/com/fredporciuncula/pager_space_issue/MainActivity.kt
+++ app/src/main/java/com/fredporciuncula/pager_space_issue/MainActivity.kt
@@ -3,23 +3,43 @@ package com.fredporciuncula.pager_space_issue
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.fredporciuncula.pager_space_issue.ui.theme.PagerspaceissueTheme
 
 class MainActivity : ComponentActivity() {
+  @OptIn(ExperimentalFoundationApi::class)
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
       PagerspaceissueTheme {
-        // A surface container using the 'background' color from the theme
-        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-          Greeting("Android")
+        HorizontalPager(
+          modifier = Modifier.fillMaxSize(),
+          state = rememberPagerState { 3 },
+          pageSpacing = 24.dp,
+        ) { page ->
+          Box(
+            modifier = Modifier
+              .fillMaxSize()
+              .background(
+                when (page) {
+                  0 -> Color.Red
+                  1 -> Color.Blue
+                  2 -> Color.Green
+                  else -> error("Invalid page")
+                }
+              )
+          )
         }
       }
     }
